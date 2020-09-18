00e7ae9d4bf5c21db41a2e3f53f9f9e5e41e1c96
Hollow9 <35386016+Hollow9@users.noreply.github.com>
2020-09-17 21:45:16 -0300

    Test para el bot

--- routes/pito.js
+++ routes/pito.js
@@ -64,7 +64,7 @@ class AWSManager {
     deleteFaces = (parametros) => {
         rekognition.deleteFaces(parametros, function (err, data) {
             if (err) console.log(err, err.stack); // an error occurred
-            else console.log(data);           // successful response
+            else console.log(data);           // successfl response
         });
     }
 
