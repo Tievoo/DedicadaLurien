79ebb58604d44b32b78a434b5ec42d072f72bcbe
Fabro0 <47898082+Fabro0@users.noreply.github.com>
2020-10-27 16:55:48 -0300

    dedicada frnt

--- aws.js
+++ aws.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {spawn} = require('child_process')
+const { spawn } = require('child_process')
 const UserNew = require('./models/User')
 AWS.config.update({ region: 'us-east-1' });
 const mongoose = require('mongoose')
@@ -42,27 +42,33 @@ app.use('/ballena', router)
 
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
-    console.log('successfully connected to database');
+  console.log('successfully connected to database');
 });
 
-app.get('/messi', async(req,res)=>{
+app.get('/messi', async (req, res) => {
   mongoose.connection.useDb("lurien").collection("usernews")
-  return res.json(await UserNew.findOne({dni:45583265}))
+  return res.json(await UserNew.findOne({ dni: 45583265 }))
 })
 
-app.get('/messi3', async(req,res)=>{
-  
+app.get('/runLurien', async (req, res) => {
+
+  try {
+    var tkn = await admin.auth().createCustomToken("baruj ata", { hidden: process.env.hidden })
+    firebase.auth().signInWithCustomToken(tkn)
+    const py = spawn('python', ['test.py', process.env.CAMERA])
+  } catch (err) {
+    return res.json({ message: err, msgError: true })
+
+  }
+
 })
 
-app.get('/messi2', async(req,res)=>{
-  await mongoose.connection.useDb("lurien").collection("entradas").deleteMany({name:"tievo"})
+app.get('/messi2', async (req, res) => {
+  await mongoose.connection.useDb("lurien").collection("entradas").deleteMany({ name: "tievo" })
   return res.json('yess')
 })
-app.listen(6969, async ()=>{
+app.listen(6969, async () => {
   console.log("boca.")
-  
-  var tkn = await admin.auth().createCustomToken("baruj ata", { hidden: process.env.hidden })
-  firebase.auth().signInWithCustomToken(tkn)
-  const py = spawn('python',['test.py', process.env.CAMERA]) 
- })
+
+})
 
